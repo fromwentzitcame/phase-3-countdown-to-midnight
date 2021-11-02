b93904373c682dca326550df843b706f028094fc
Alex Wentz <wentz.alex@gmail.com>
2021-11-02 13:32:09 -0400

    commit tree 4199dde5a4a71de524957a9806b95c3761318e60

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(int)
+    i = int.to_i
+    while i > 0
+        puts "#{i} SECOND(S)!"
+        i -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+    i = int.to_i
+    while i > 0
+        puts "#{i} SECOND(S)!"
+        i -= 1
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
+end
+
